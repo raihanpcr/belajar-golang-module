8a818825501c9aa6cebf02bad5af76fdd13bc34c
raihanpcr <raihanalrasyid15@gmail.com>
2024-03-19 00:54:41 +0700

    update say hello

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 //cara menggunakan module go mod ini packagenya
 
+// cara menambahkan module "go get nama-module"
 package gosayhello
 
 func SayHello() string {
-	return "Hello"
+	return "Hello World"
 }
+
+//update module
